c9dfe49405ec378fcf3b2d4d12b2a9e8841bb33a
ningth <ningth_jn@163.com>
2020-12-30 21:35:34 +0800

    temp

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,15 +4,14 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
-class LivesObj {
+class DataObj {
 
 	public String str;
 }
 
-class ForecastObj {
+class LivesObj extends DataObj {}
 
-	public String str;
-}
+class ForecastObj extends DataObj {}
 
 class MyWeather {
 
@@ -22,53 +21,19 @@ class MyWeather {
 		.connectTimeout(Duration.ofSeconds(10))
 		.build();
 
-	String key = "7f76ae97dfa7bfbb32a22cf8e0b9c052";
-	String forecastFormat =
-		"https://restapi.amap.com/v3/weather/weatherInfo?key=%s&extensions=all&city=%s";
-	String livesFormat =
-		"https://restapi.amap.com/v3/weather/weatherInfo?key=%s&extensions=base&city=%s";
-
 	public MyWeather() {}
 
-	public MyWeather(String _key) {
-		key = _key;
-	}
-
-	public boolean getLivesWeather(String cityCode, LivesObj result) {
+	public boolean GetWeather(String url, DataObj result) {
 		try {
 			// 创建一个自定义的HTTP请求对象
 			HttpRequest request = HttpRequest
 				.newBuilder()
 				.GET()
-				.uri(URI.create(String.format(livesFormat, this.key, cityCode)))
+				.uri(URI.create(url))
 				.build();
 
 			HttpResponse<String> response =
 				this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-			if (response.statusCode() == 200) {
-				result.str = response.body();
-				return true;
-			}
-		} catch (Exception e) {
-			System.out.println("error---> " + e.getMessage());
-		}
-
-		return false;
-	}
-
-	public boolean getForecastWeather(String cityCode, ForecastObj result) {
-		try {
-			// 创建一个自定义的HTTP请求对象
-			HttpRequest request = HttpRequest
-				.newBuilder()
-				.GET()
-				.uri(URI.create(String.format(forecastFormat, this.key, cityCode)))
-				.build();
-
-			HttpResponse<String> response = httpClient.send(
-				request,
-				HttpResponse.BodyHandlers.ofString()
-			);
 			if (response.statusCode() == 200) {
 				result.str = response.body();
 				return true;
@@ -88,14 +53,29 @@ public class App {
 		LivesObj livesObj = new LivesObj();
 		ForecastObj forecastObj = new ForecastObj();
 
+		String key = "7f76ae97dfa7bfbb32a22cf8e0b9c052";
+		String cityCode = "110101";
+		String forecastFormat =
+			"https://restapi.amap.com/v3/weather/weatherInfo?key=%s&extensions=all&city=%s";
+		String livesFormat =
+			"https://restapi.amap.com/v3/weather/weatherInfo?key=%s&extensions=base&city=%s";
+
 		// 获取实时天气
-		if (myWeather.getLivesWeather("110101", livesObj)) {
+		if (
+			myWeather.GetWeather(String.format(livesFormat, key, cityCode), livesObj)
+		) {
 			System.out.println(livesObj.str);
 		}
 
 		// 获取3天预报
-		if (myWeather.getForecastWeather("110101", forecastObj)) {
+		if (
+			myWeather.GetWeather(
+				String.format(forecastFormat, key, cityCode),
+				forecastObj
+			)
+		) {
 			System.out.println(forecastObj.str);
 		}
+		// 1、函数参数有引用吗， 类似与c#中的ref，如： int i; func(ref i)
 	}
 }
